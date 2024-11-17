267417d5d5b81496bda202489c4ace62ac91b34e
Ten Denis <denis.a.ten@gmail.com>
2024-11-17 17:34:23 +0100

    1111

--- src/count-vm.tf
+++ src/count-vm.tf
@@ -1,14 +1,14 @@
 resource "yandex_compute_instance" "web" {
   count = 2
 
-  name = "web-${count.index + 1}"  # Создаст web-1 и web-2
-  zone = "ru-central1-a"     
+  name = "web-${count.index + 1}" # Создаст web-1 и web-2
+  zone = "ru-central1-a"
 
   resources {
     core_fraction = 5
-    cores  = 2
-    memory = 1
-    
+    cores         = 2
+    memory        = 1
+
   }
 
   boot_disk {
@@ -18,19 +18,20 @@ resource "yandex_compute_instance" "web" {
   }
 
   network_interface {
-    subnet_id = "e9bi11del4gshgcmh476"
-    nat = true
+    subnet_id          = "e9bi11del4gshgcmh476"
+    nat                = true
     security_group_ids = ["enpgkfejf10nncf6u3p1"]
   }
-  
+
   scheduling_policy {
-    preemptible = true  # Указывает, что ВМ может быть прерываемой
+    preemptible = true # Указывает, что ВМ может быть прерываемой
   }
-  
+
   metadata = {
-    ssh-keys = "lamos:${local.ssh_public_key}"
+    ssh-keys  = "lamos:${local.ssh_public_key}"
     user-data = "${file("cloud-init.yaml")}"
   }
 
+  depends_on = [yandex_compute_instance.db] # Зависимость от ВМ db
 }
 
--- src/locals.tf
+++ src/locals.tf
@@ -15,8 +15,7 @@ locals {
       zone        = "ru-central1-a"
     }
   ]
-depends_on = [yandex_compute_instance.db]  # Зависимость от ВМ db
-}
+
   ssh_public_key = file("/root/.ssh/id_ed25519.pub")
 }
 
