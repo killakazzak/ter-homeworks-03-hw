c2899dd3833755dae1cfbb7eb617f3ba01d3641f
Ten Denis <denis.a.ten@gmail.com>
2024-11-19 14:49:01 +0100

    modified:   main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,9 +1,40 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
 }
+
+output "vm_list" {
+  value = concat(
+    [
+      for i in range(var.instance_count) : {
+        name = "web-${i + 1}"
+        id   = yandex_compute_instance.web[i].id
+        fqdn = "${yandex_compute_instance.web[i].id}.auto.internal"
+      }
+    ],
+    [
+      for instance in yandex_compute_instance.db : {
+        name = instance.name
+        id   = instance.id
+        fqdn = "${instance.id}.auto.internal"
+      }
+    ],
+    [
+      {
+        name = yandex_compute_instance.storage.name
+        id   = yandex_compute_instance.storage.id
+        fqdn = "${yandex_compute_instance.storage.id}.auto.internal"
+      }
+    ]
+  )
+}
+
+
+
+
